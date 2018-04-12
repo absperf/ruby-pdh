c39c9d3cb1d1c7bd308d6f569d6dd9872de325a1
Taylor C. Richberger <Taywee@gmx.com>
2018-04-12 14:55:25 -0600

    tweak method calls

--- lib/win32/pdh.rb
+++ lib/win32/pdh.rb
@@ -1,4 +1,6 @@
-require 'win32/pdh/constants'
+#require 'win32/pdh/constants'
+
+require 'ffi'
 
 module Win32
   module PdhFFI
@@ -9,6 +11,12 @@ module Win32
     typedef :pointer, :pdh_hcounter
     typedef :uint, :pdh_status
 
-    attach_function :PdhOpenQueryA, [:string, :pointer, :pointer], :pdh_status
+    attach_function :PdhOpenQueryW, [:buffer_in, :buffer_in, :buffer_out], :pdh_status
+
+    ##
+    # Simple query opening function.  Uses the OpenQuery function and gets a
+    # query, passes it into the block, and then closes it afterward.
+    def self.query
+    end
   end
 end
