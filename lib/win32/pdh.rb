fa5f6974365ec217266aa06e132fa10b1a9373f9
Taylor C. Richberger <Taywee@gmx.com>
2018-04-15 15:15:23 -0600

    add wildcard expansion

--- lib/win32/pdh.rb
+++ lib/win32/pdh.rb
@@ -4,9 +4,13 @@ require 'ffi'
 
 module Win32
   module Pdh
+    class PdhError < StandardError
+    end
+
     module PdhFFI
       extend FFI::Library
-      ffi_lib :Pdh
+      ffi_lib 'Pdh.dll'
+      ffi_convention :stdcall
 
       typedef :pointer, :pdh_hquery
       typedef :pointer, :pdh_hcounter
@@ -18,6 +22,7 @@ module Win32
       attach_function :PdhCloseQuery, [:buffer_in], :pdh_status
       attach_function :PdhEnumObjectsW, [:buffer_in, :buffer_in, :buffer_out, :buffer_inout, :uint, :winbool], :pdh_status
       attach_function :PdhEnumObjectItemsW, [:buffer_in, :buffer_in, :buffer_in, :buffer_out, :buffer_inout, :buffer_out, :buffer_inout, :uint, :uint], :pdh_status
+      attach_function :PdhExpandWildCardPathW, [:buffer_in, :buffer_in, :buffer_out, :buffer_inout, :uint], :pdh_status
     end
 
     ##
@@ -53,34 +58,26 @@ module Win32
           Constants::PERF_DETAIL_NOVICE
         end
 
-      # TODO: error handling
+      listsize = FFI::MemoryPointer.new(:uint)
+      listsize.write_uint(0)
+      listbuffer = FFI::Pointer::NULL
+      status = nil
+      while status.nil? || status == Constants::PDH_MORE_DATA
+        listbuffer = FFI::Buffer.new(:uint16, listsize.read_uint) unless status.nil?
 
-      # First get the required size
-      bufsize = FFI::MemoryPointer.new(:uint)
-      bufsize.write_uint(0)
-      PdhFFI.PdhEnumObjectsW(
-        source,
-        machine,
-        FFI::Pointer::NULL,
-        bufsize,
-        detail,
-        1,
-      )
+        status = PdhFFI.PdhEnumObjectsW(
+          source,
+          machine,
+          listbuffer,
+          listsize,
+          detail,
+          status.nil? ? 1 : 0,
+        )
+      end
 
-      # Allocate the buffer
-      buffer = FFI::Buffer.new(:uint16, bufsize.read_uint)
+      raise PdhError, "PDH error #{Constants::LOOKUP[status]}!" unless status == Constants::ERROR_SUCCESS
 
-      # Fill the buffer
-      PdhFFI.PdhEnumObjectsW(
-        source,
-        machine,
-        buffer,
-        bufsize,
-        detail,
-        0,
-      )
-
-      string = buffer.read_bytes(bufsize.read_uint * 2).force_encoding('UTF-16LE').encode('UTF-8')
+      string = listbuffer.read_bytes(listsize.read_uint * 2).force_encoding('UTF-16LE').encode('UTF-8')
 
       # Split and return objects
       string.split("\0")
@@ -142,8 +139,7 @@ module Win32
         )
       end
 
-      # TODO: improve error handling
-      raise "PDH error #{Constants::LOOKUP[status]}!" unless status == Constants::ERROR_SUCCESS
+      raise PdhError, "PDH error #{Constants::LOOKUP[status]}!" unless status == Constants::ERROR_SUCCESS
 
       counterstring = counterbuffer.read_bytes(countersize.read_uint * 2).force_encoding('UTF-16LE').encode('UTF-8')
       instancestring = instancebuffer.read_bytes(instancesize.read_uint * 2).force_encoding('UTF-16LE').encode('UTF-8')
@@ -152,6 +148,43 @@ module Win32
       enum.counters = counterstring.split("\0").map(&:freeze).freeze
       enum.instances = instancestring.split("\0").map(&:freeze).freeze
       enum.freeze
+    end
+
+    ##
+    # Expands a wildcard path into all matching counter paths.
+    def self.expand_wildcards(path:, source: nil, expand_counters: true, expand_instances: true)
+      path = (path + "\0").encode('UTF-16LE')
+      source =
+        if source.nil?
+          FFI::Pointer::NULL
+        else
+          (source + "\0").encode('UTF-16LE')
+        end
+
+      flags = 0
+      flags |= PDH_NOEXPANDCOUNTERS unless expand_counters
+      flags |= PDH_NOEXPANDINSTANCES unless expand_instances
+
+      listsize = FFI::MemoryPointer.new(:uint)
+      listsize.write_uint(0)
+      listbuffer = FFI::Pointer::NULL
+      status = nil
+      while status.nil? || status == Constants::PDH_MORE_DATA
+        listbuffer = FFI::Buffer.new(:uint16, listsize.read_uint) unless status.nil?
+        status = PdhFFI.PdhExpandWildCardPathW(
+          source,
+          path,
+          listbuffer,
+          listsize,
+          flags,
+        )
+      end
+
+      raise PdhError, "PDH error #{Constants::LOOKUP[status]}!" unless status == Constants::ERROR_SUCCESS
+
+      liststring = listbuffer.read_bytes(listsize.read_uint * 2).force_encoding('UTF-16LE').encode('UTF-8')
+
+      liststring.split("\0").map(&:freeze).freeze
     end
   end
 end
